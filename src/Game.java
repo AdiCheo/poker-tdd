51604e36e02fd6a1f38ea36a01485302c3103dd1
Adi Cheo <adi.cheo@gmail.com>
2015-09-26 17:59:16 -0400

    Add question for number of players

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,7 @@
 
 public class Game {
+	public Game(){
+		System.out.println("Welcome to PokerTDD - How many players are present?");
+	}
 
 }
--- src/GameTest.java
+++ src/GameTest.java
@@ -1,20 +1,39 @@
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class GameTest {
 	
 	Game pokerGame;
+	
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
 	@Before
 	public void setUp() throws Exception {
+	    System.setOut(new PrintStream(outContent));
+	    
+		pokerGame = new Game();
 	}
 
 	@Test
 	public void testCreateNewGame() {
-		pokerGame = new Game();
 		assertNotNull(pokerGame);
+	}
+
+	@Test
+	public void testNewGameAsksForNomberOfPlayer() {
+	    assertEquals("Welcome to PokerTDD - How many players are present?", outContent.toString().trim());
+	}	
+	
+	
+	@After
+	public void cleanUpStreams() {
+	    System.setOut(null);
 	}
 
 }

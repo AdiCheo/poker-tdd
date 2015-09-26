86e7a1a15cf97e8bda4cedb777a15773a38b6434
Adi Cheo <adi.cheo@gmail.com>
2015-09-26 18:26:48 -0400

    Add test getPlayerId

--- src/PlayerTest.java
+++ src/PlayerTest.java
@@ -4,16 +4,20 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class PlayerTest {
+	Player testObject;
 
 	@Before
 	public void setUp() throws Exception {
+		testObject = new Player();
 	}
 
 	@Test
-	public void test() {
-		Player testObject;
-		testObject = new Player();
+	public void testCreatePlayer() {
 		assertNotNull(testObject);
 	}
 
+	@Test
+	public void testGetPlayerId() {
+		assertFalse(testObject.getId() < 5);
+	}
 }

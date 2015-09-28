ce3a124e5ff4108b563f085beded8e5392066220
Adi Cheo <adi.cheo@gmail.com>
2015-09-28 01:26:07 -0400

    remove Game() constructor since we always want to pass player number to Game

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,8 @@
 import java.util.ArrayList;
 
 public class Game {
-	public Game(){
-		System.out.println("Welcome to PokerTDD - How many players are present?");
-	}
-
 	public Game(int p) {
+		System.out.println("Welcome to PokerTDD - How many players are present?");
 		for (int i=0;i<p;i++){
 			players.add(new Player());
 		}
